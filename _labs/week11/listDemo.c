30c38112bbfadd3dc825716a573024cddab1fad8
Alex Hinds <alex.hinds141@gmail.com>
2017-10-13 15:25:37 +1100

    with msot recent fiels

--- _labs/week11/listDemo.c
+++ _labs/week11/listDemo.c
@@ -48,9 +48,172 @@ void deleteList(List l) {
    free(l);
 }
 
-void append(List l, int item);
-void prepend(List l, int item);
-void remove(List l, int item);
-int count(List l);
-int contains(int item);
-void printList(List l);
+void appendRecursive(Node n, int item) {
+   if (n->next) {
+      appendRecursive(n, item);
+   } else {
+      n->next = newNode(item);
+   }
+}
+
+void append(List l, int item) {
+   // if the list is empty
+   if (l->head == NULL) {
+      l->head = newNode(item);
+   } else {
+      Node curr = l->head;
+      while (curr->next != NULL) {
+         curr = curr->next;
+      }
+      curr->next = newNode(item);
+   }
+}
+
+void prepend(List l, int item) {
+   Node new = newNode(item);
+   new->next = l->head;
+   l->head = new;
+}
+
+void remove(List l, int item) {
+
+   // 1
+   if (l->head == NULL) {
+      // do nothing.
+   } else if (l->head) {
+      Node curr = l->head;
+      Node prev = curr;
+      // 1 item case
+      if (l->head->next == NULL) {
+
+      // all other items case
+      } else {
+         int flag = 1;
+         while (curr->next && flag) {
+
+            if (curr->data == item) {
+               // if this is the first item
+               if (curr == prev) {
+                  l->head = curr->next;
+               } else {
+                  prev->next = curr->next;
+               }
+               deleteNode(curr);
+               flag = 0;
+            }
+
+            if (flag == 1) {
+               prev = curr;
+               curr = curr->next;
+            }
+         }
+
+      }
+   }
+}
+
+int countHelper(Node n) {
+   if (n != NULL) {
+      return 1 + countHelper(n->next);
+   } else {
+      return 0;
+   }
+}
+
+int count(List l) {
+   if (l->head != NULL) {
+      // 1 + counter( 1 + counter (1 + counter))....>
+      return countHelper(l->head);
+   } else {
+      return 0;
+   }
+}
+
+int count(List l) {
+   Node curr = l->head;
+   int counter = 0;
+   while (curr != NULL) {
+      curr = curr->next;
+      counter++;
+   }
+   return counter;
+}
+
+int contains(List l, int item) {
+   Node curr = l->head;
+
+   while (curr) {
+      if (curr->data == item) {
+         return TRUE;
+      }
+      curr = curr->next;
+   }
+   return FALSE;
+}
+
+void deleteTail(List l) {
+   l->head = deleteEnd(l->head);
+}
+
+Node deleteEnd(Node n) {
+   if (n == NULL) {
+      return NULL;
+   } else if (n->next == NULL) {
+      free(n);
+      return NULL;
+   } else {
+      n->next = deleteEnd(n->next);
+      return n;
+   }
+}
+
+void insertNth(List l, int n, int item) {
+
+   Node curr = l->head;
+   int counter = 1;
+
+   if (curr == NULL) {
+      l->head = newNode(item);
+   } else {
+      if (n == 0) {
+         Node new = newNode(item);
+         new->next = l->head->next;
+         l->head = new;
+      } else {
+         while (counter < n) {
+            curr = curr->next;
+            counter++;
+         }
+         Node new = newNode(item);
+         new->next = curr->next;
+         curr->next = new;
+      }
+  }
+}
+
+
+void insertNth(List l, int n, int item) {
+   l->head = insertNthHelper(l->head, n, 0, item);
+}
+
+
+Node insertNthHelper(Node n, int n, int curr, int item) {
+   if (n == curr) {
+      Node new = newNode(item);
+      new->next = n->next;
+      n->next = new;
+      return new;
+   } else {
+      n->next = insertNthHelper(n->next, n, curr+1, item);
+      return n;
+   }
+}
+
+void printList(List l) {
+   Node curr = l->head;
+   while (curr) {
+      printf("[%d]->",curr->data);
+      curr = curr->next;
+   }
+   printf("[X]\n");
+}
---
+++ _labs/week11/test.c
@@ -0,0 +1,9 @@
+#include "listDemo.h"
+
+
+#ifdef DEBUG
+int main(int argc, char const *argv[]) {
+   /* code */
+   return 0;
+}
+#endif
